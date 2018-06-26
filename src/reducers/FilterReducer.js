c27b547b8c3fab5ca6e788becbd109d24885b276
Nuno Miguel Almeida Soares <nuno.soares@ua.pt>
2018-06-26 16:04:44 +0100

    done except read-me

--- src/reducers/FilterReducer.js
+++ src/reducers/FilterReducer.js
@@ -1,12 +1,15 @@
-import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE, SET_VISIBILITY_FILTER} from '../actions/actionsTypes'
+import {
+  SHOW_ALL,
+  SET_VISIBILITY_FILTER
+} from "../actions/actionsTypes";
 
 const visibilityFilter = (state = SHOW_ALL, action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
-      return action.filter
+      return action.filter;
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default visibilityFilter
+export default visibilityFilter;
